d0d3abf86b2ded573f684db478f71c4d68d416c4
Joseph <joseph@appreciateco.com>
2019-04-29 05:35:36 -0700

    fixed the position of the lightbox based on viewport

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,22 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    libraryTarget: 'commonjs2'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",query: {compact: true}
-        }
-      }
-    ]
-  }
-  /*,
-  externals: {
-    'react': 'commonjs react', // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-    'axios': 'commonjs axios'  
-  }*/
-};
+	entry: './index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+		libraryTarget: 'commonjs2'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					query: { compact: true }
+				}
+			}
+		]
+	}
+}
